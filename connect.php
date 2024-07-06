cc97b141918229ed06481ba0c682bde44fbbbb4e
breadO <87787817+yukiiHunter@users.noreply.github.com>
2024-07-06 20:45:46 +0700

    Update connect.php with pdo quert

--- connect.php
+++ connect.php
@@ -5,6 +5,12 @@ $db_user = "root";
 $db_pass = "";
 $db_name = "daftar_ulang";
 
-$conn = mysqli_connect($db_host, $db_user , $db_pass, $db_name);
+try {
+    $pdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
 
 ?>
