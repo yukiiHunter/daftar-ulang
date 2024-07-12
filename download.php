b21831e6d2cd28b29801764c41d6e5ca39dda179
breadO <87787817+yukiiHunter@users.noreply.github.com>
2024-07-12 16:09:17 +0700

    Change download method

--- download.php
+++ download.php
@@ -1,91 +1,77 @@
 <?php
 
-include 'tcpdf/tcpdf.php';
-include 'fpdi/src/autoload.php';
 include 'connect.php';
+require_once('vendor/autoload.php');
 
-use setasign\Fpdi\Tcpdf\Fpdi;
+// Query untuk mengambil data
+$query = $pdo->query('SELECT * FROM input_data');
 
-if (isset($_GET['no'])) {
-    $no = intval($_GET['no']);
+// Buat objek PDF baru
+$pdf = new TCPDF();
 
-    // Fetch data from database
-    $stmt = $conn->prepare("SELECT * FROM input_data WHERE no = ?");
-    $stmt->bind_param("i", $no);
-    $stmt->execute();
-    $result = $stmt->get_result();
-    $data = $result->fetch_assoc();
+// Atur informasi dokumen
+$pdf->SetCreator(PDF_CREATOR);
+$pdf->SetAuthor('Nama Anda');
+$pdf->SetTitle('Data Siswa');
+$pdf->SetSubject('Export Data Siswa');
 
-    if ($data) {
-        // Path to the template PDF
-        $templateFile = 'assets/template.pdf'; // Replace with your template PDF path
+// Atur margin
+$pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
 
-        // Create new PDF instance
-        $pdf = new Fpdi();
+// Tambah halaman baru
+$pdf->AddPage();
 
-        // Add a page
-        $pdf->AddPage();
+// Atur font
+$pdf->SetFont('Times', '', 10);
 
-        // Set source template
-        $pageCount = $pdf->setSourceFile($templateFile);
-        $tplIdx = $pdf->importPage(1);
+// Menulis data siswa
+$html = '<h1>Data Siswa</h1>';
 
-        // Use the imported page
-        $pdf->useTemplate($tplIdx, 0, 0, null, null, true);
-
-        // Set font
-        $pdf->SetFont('Times New Roman', '', 12);
-
-        // Get the content of the template PDF
-        $pdfText = file_get_contents($templateFile);
-
-        // Replace placeholders in the template content
-        $ttl = $data['tempat_lahir'] . "," . $data['tanggal_lahir'];
-
-        $pdfText = str_replace('{{nisn}}', $data['nisn'], $pdfText);
-        $pdfText = str_replace('{{program}}', $data['program'], $pdfText);
-        $pdfText = str_replace('{{nama}}', $data['nama_siswa'], $pdfText);
-        $pdfText = str_replace('{{kelamin}}', $data['kelamin'], $pdfText);
-        $pdfText = str_replace('{{ttl}}', $ttl, $pdfText);
-        $pdfText = str_replace('{{agama}}', $data['agama'], $pdfText);
-        $pdfText = str_replace('{{alamat}}', $data['alamat'], $pdfText);
-        $pdfText = str_replace('{{kota}}', $data['kota'], $pdfText);
-        $pdfText = str_replace('{{email}}', $data['email'], $pdfText);
-        $pdfText = str_replace('{{nik}}', $data['nik'], $pdfText);
-        $pdfText = str_replace('{{kk}}', $data['no_kk'], $pdfText);
-        $pdfText = str_replace('{{golDar}}', $data['golongan_darah'], $pdfText);
-        $pdfText = str_replace('{{bb}}', $data['berat_badan'], $pdfText);
-        $pdfText = str_replace('{{tinggi}}', $data['tinggi_badan'], $pdfText);
-        $pdfText = str_replace('{{lingkarKepala}}', $data['lingkar_kepala'], $pdfText);
-        $pdfText = str_replace('{{jarak}}', $data['jarak_rumah'], $pdfText);
-        $pdfText = str_replace('{{asal}}', $data['asal_sekolah'], $pdfText);
-        $pdfText = str_replace('{{skhu}}', $data['no_skhu'], $pdfText);
-        $pdfText = str_replace('{{status}}', $data['status_di_keluarga'], $pdfText);
-        $pdfText = str_replace('{{anakKe}}', $data['anak_ke'], $pdfText);
-        $pdfText = str_replace('{{tanggungan}}', $data['tanggungan_keluarga'], $pdfText);
-        $pdfText = str_replace('{{ayah}}', $data['nama_ayah'], $pdfText);
-        $pdfText = str_replace('{{nikAyah}}', $data['nik_ayah'], $pdfText);
-        $pdfText = str_replace('{{jobAyah}}', $data['pekerjaan_ayah'], $pdfText);
-        $pdfText = str_replace('{{salaryAyah}}', $data['penghasilan_ayah'], $pdfText);
-        $pdfText = str_replace('{{ibu}}', $data['nama_ibu'], $pdfText);
-        $pdfText = str_replace('{{nikIbu}}', $data['nik_ibu'], $pdfText);
-        $pdfText = str_replace('{{jobIbu}}', $data['pekerjaan_ibu'], $pdfText);
-        $pdfText = str_replace('{{salaryIbu}}', $data['penghasilan_ibu'], $pdfText);
-        $pdfText = str_replace('{{alamatOrtu}}', $data['alamat_ortu'], $pdfText);
-        $pdfText = str_replace('{{telpOrtu}}', $data['no_telp_ortu'], $pdfText);
-        $pdfText = str_replace('{{wali}}', $data['nama_wali'], $pdfText);
-        $pdfText = str_replace('{{nikWali}}', $data['nik_wali'], $pdfText);
-        $pdfText = str_replace('{{jobWali}}', $data['pekerjaan_wali'], $pdfText);
-        $pdfText = str_replace('{{salaryWali}}', $data['penghasilan_wali'], $pdfText);
-        $pdfText = str_replace('{{alamatWali}}', $data['alamat_wali'], $pdfText);
-        $pdfText = str_replace('{{telpWali}}', $data['no_telp_wali'], $pdfText);
-
-        // Output the PDF
-        $pdf->Output('member_data_' . $no . '.pdf', 'D');
-    } else {
-        echo "Data not found for the given ID.";
-    }
-} else {
-    echo "No ID provided.";
+while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
+    $html .= '<hr><h2>Siswa</h2>';
+    $html .= '<p><strong>Program:</strong> ' . $row['program'] . '</p>';
+    $html .= '<p><strong>Nama:</strong> ' . $row['nama_siswa'] . '</p>';
+    $html .= '<p><strong>Kelamin:</strong> ' . $row['kelamin'] . '</p>';
+    $html .= '<p><strong>Agama:</strong> ' . $row['agama'] . '</p>';
+    $html .= '<p><strong>NISN:</strong> ' . $row['nisn'] . '</p>';
+    $html .= '<p><strong>NIS:</strong> ' . $row['nis'] . '</p>';
+    $html .= '<p><strong>Tempat Lahir:</strong> ' . $row['tempat_lahir'] . '</p>';
+    $html .= '<p><strong>Tanggal Lahir:</strong> ' . $row['tanggal_lahir'] . '</p>';
+    $html .= '<p><strong>Alamat Siswa:</strong> ' . $row['alamat'] . '</p>';
+    $html .= '<p><strong>Kota Tempat Tinggal:</strong> ' . $row['kota'] . '</p>';
+    $html .= '<p><strong>No HP Siswa:</strong> ' . $row['telp'] . '</p>';
+    $html .= '<p><strong>Email:</strong> ' . $row['email'] . '</p>';
+    $html .= '<p><strong>NIK:</strong> ' . $row['nik'] . '</p>';
+    $html .= '<p><strong>No KK:</strong> ' . $row['no_kk'] . '</p>';
+    $html .= '<p><strong>Golongan Darah:</strong> ' . $row['golongan_darah'] . '</p>';
+    $html .= '<p><strong>Berat Badan (kg):</strong> ' . $row['berat_badan'] . '</p>';
+    $html .= '<p><strong>Lingkar Kepala (cm):</strong> ' . $row['lingkar_kepala'] . '</p>';
+    $html .= '<p><strong>Jarak Rumah ke Sekolah:</strong> ' . $row['jarak_rumah'] . '</p>';
+    $html .= '<p><strong>Asal Sekolah:</strong> ' . $row['asal_sekolah'] . '</p>';
+    $html .= '<p><strong>No SKHU:</strong> ' . $row['no_skhu'] . '</p>';
+    $html .= '<p><strong>Status dalam Keluarga:</strong> ' . $row['status_di_keluarga'] . '</p>';
+    $html .= '<p><strong>Anak Ke:</strong> ' . $row['anak_ke'] . '</p>';
+    $html .= '<p><strong>Tanggungan Keluarga:</strong> ' . $row['tanggungan_keluarga'] . '</p>';
+    $html .= '<p><strong>Nama Ayah:</strong> ' . $row['nama_ayah'] . '</p>';
+    $html .= '<p><strong>NIK Ayah:</strong> ' . $row['nik_ayah'] . '</p>';
+    $html .= '<p><strong>Pekerjaan Ayah:</strong> ' . $row['pekerjaan_ayah'] . '</p>';
+    $html .= '<p><strong>Penghasilan Ayah:</strong> ' . $row['penghasilan_ayah'] . '</p>';
+    $html .= '<p><strong>Nama Ibu:</strong> ' . $row['nama_ibu'] . '</p>';
+    $html .= '<p><strong>NIK Ibu:</strong> ' . $row['nik_ibu'] . '</p>';
+    $html .= '<p><strong>Pekerjaan Ibu:</strong> ' . $row['pekerjaan_ibu'] . '</p>';
+    $html .= '<p><strong>Penghasilan Ibu:</strong> ' . $row['penghasilan_ibu'] . '</p>';
+    $html .= '<p><strong>Alamat Orang Tua:</strong> ' . $row['alamat_ortu'] . '</p>';
+    $html .= '<p><strong>No Telpon Orang Tua:</strong> ' . $row['alamat_ortu'] . '</p>';
+    $html .= '<p><strong>Nama Wali:</strong> ' . $row['nama_wali'] . '</p>';
+    $html .= '<p><strong>NIK Wali:</strong> ' . $row['nik_wali'] . '</p>';
+    $html .= '<p><strong>Pekerjaan Wali:</strong> ' . $row['pekerjaan_wali'] . '</p>';
+    $html .= '<p><strong>Penghasilan Wali:</strong> ' . $row['penghasilan_wali'] . '</p>';
+    $html .= '<p><strong>Alamat Wali:</strong> ' . $row['alamat_wali'] . '</p>';
+    $html .= '<p><strong>No Telepon Wali:</strong> ' . $row['no_telp_wali'] . '</p>';
 }
-?>
+
+// Tulis HTML ke PDF
+$pdf->writeHTML($html, true, false, true, false, '');
+
+// Output PDF
+$pdf->Output('data_siswa.pdf', 'I');
